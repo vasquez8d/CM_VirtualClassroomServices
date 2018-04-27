7bdd5c96b692ab81ccf7d99afc7fde05ba99f5ea
Alexsandre Vasquez Malpartida <norman.vasquez-ext@financierauno.pe>
2018-04-27 17:51:21 -0500

    examen

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -74,5 +74,107 @@ module.exports = {
             }
         });
     },
+    test: function(req, res){
+        var dataResponse = {
+            data_result: "",
+            res_service: "",
+            des_error: ""
+        };
+        var jwtKey = sails.config.values.jwtkey;
+        var dataToken = req.headers.authorization;
+        jwt.verify(dataToken, jwtKey, function (err, decoded) {
+            if (err) {
+                dataResponse.res_service = "401 unauthorized";
+                dataResponse.des_error = err;
+                res.json(dataResponse);
+            } else {
+                var dataTest = req.allParams();
+                var dataTestFinal = [];
+
+                if (dataTest.test_type_id == 1 || dataTest.test_type_id == 2){
+                    
+                    var numQuesSaludPublic = parseInt(dataTest.test_num_ques * 0.1);
+                    var numQuesCiencBasics = parseInt(dataTest.test_num_ques * 0.1);
+                    var numQuesGinecologia = parseInt(dataTest.test_num_ques * 0.2);
+                    var numQuesPediatria   = parseInt(dataTest.test_num_ques * 0.2);
+                    var numQuesCirugia     = parseInt(dataTest.test_num_ques * 0.2);
+                    var numQuesMedicGenerl = parseInt(dataTest.test_num_ques * 0.2);
+                    
+                    var totQuestions = numQuesSaludPublic + numQuesCiencBasics + numQuesGinecologia +
+                                       numQuesPediatria + numQuesCirugia + numQuesMedicGenerl;
+
+                    if (totQuestions > dataTest.test_num_ques){
+                        numQuesMedicGenerl = numQuesMedicGenerl - (totQuestions - dataTest.test_num_ques);
+                    } else if (totQuestions < dataTest.test_num_ques){
+                        numQuesMedicGenerl = numQuesMedicGenerl + (dataTest.test_num_ques - totQuestions);
+                    }
+
+                    var querySaludPublica = 'select * from tbl_data_questions_det p where p.cat_cor_id = 13 order by random() limit ' + numQuesSaludPublic;
+                    var queryCienciBasics = 'select * from tbl_data_questions_det p where p.cat_cor_id = 10 order by random() limit ' + numQuesCiencBasics;
+                    var queryGinecologia  = 'select * from tbl_data_questions_det p where p.cat_cor_id = 15 order by random() limit ' + numQuesGinecologia;
+                    var queryPediatria    = 'select * from tbl_data_questions_det p where p.cat_cor_id = 16 order by random() limit ' + numQuesPediatria;
+                    var queryCirugia      = 'select * from tbl_data_questions_det p where p.cat_cor_id = 17 order by random() limit ' + numQuesCirugia;
+                    var queryMedicGeneral = 'select * from tbl_data_questions_det p where p.cat_cor_id = 18 order by random() limit ' + numQuesMedicGenerl;
+
+                    for(var i=0 ; i<6 ; i++){
+
+                        var queryTest = '';
+
+                        switch(i){
+                            case 0:
+                                queryTest = querySaludPublica;
+                                break;
+                            case 1:
+                                queryTest = queryCienciBasics;
+                                break;
+                            case 2:
+                                queryTest = queryGinecologia;
+                                break;
+                            case 3:
+                                queryTest = queryPediatria;
+                                break;
+                            case 4:
+                                queryTest = queryCirugia;
+                                break;
+                            case 5:
+                                queryTest = queryMedicGeneral;
+                                break;
+                        }
+
+                        Questions.query(queryTest, function (err, result) {
+                            if (err) {
+                                dataResponse.res_service = "Error creando examen";
+                                dataResponse.des_error = err;
+                            }
+                            if (result.rows.length > 0) {
+                                result.rows.forEach(element => {
+                                    dataTestFinal.push(element);
+                                });
+                            } else {
+                                dataResponse.res_service = "No existe información.";
+                            }
+                        });
+                    }
+                    
+                } else if(dataTest.test_type_id == 3) {
+                    
+                } else if (dataTest.test_type_id == 4) {
+
+                } else if (dataTest.test_type_id == 5) {
+
+                } else if (dataTest.test_type_id == 6) {
+
+                } else if (dataTest.test_type_id == 7) {
+
+                } else if (dataTest.test_type_id == 8) {
+
+                }
+
+                dataResponse.data_result = result.rows;
+                dataResponse.res_service = "ok";
+                res.json(dataResponse)
+            }
+        });
+    }
 };
 
